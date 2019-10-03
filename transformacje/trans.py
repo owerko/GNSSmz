9c91fff34a7838fe040faa0675ba929debc7dd1b
Tomek <owerko@gmail.com>
2019-10-03 19:59:17 +0200

    added function to transform form WGS to 2000

--- transformacje/trans.py
+++ transformacje/trans.py
@@ -1,1 +1,10 @@
-print('Hello')
+from pyproj import Proj, transform
+
+
+def trans_wgs84to2000(x, y):  # argumaty: dlugosc, szerokosc w stopniach
+    inProj = Proj(init='epsg:4326')  # WGS 84
+    outProj = Proj(init='epsg:2178')  # Uklad 2000 strefa 7
+    return transform(inProj, outProj, x, y)
+
+
+print(trans_wgs84to2000(20, 50))
