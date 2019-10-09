0425083c91282c17c228bab16666b2d85274e84c
Tomek <owerko@gmail.com>
2019-10-09 14:05:19 +0200

    added 2000 to wgs

--- transformacje/trans.py
+++ transformacje/trans.py
@@ -6,6 +6,8 @@ def trans_wgs84to2000(x, y):  # argumaty: dlugosc, szerokosc w stopniach
     outProj = Proj(init='epsg:2178')  # Uklad 2000 strefa 7
     return transform(inProj, outProj, x, y)
 
+# dd mm ss.sssss -> dd+mm/60+ss.sssss/36000
+
 
 def trans_2000towgs84(x, y):  # argumaty: dlugosc, szerokosc w stopniach
     outProj = Proj(init='epsg:4326')  # WGS 84
