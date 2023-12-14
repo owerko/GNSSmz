80d3be174e06099a7ed2204b59e6acccb2a18977
owerko <owerko@gmail.com>
2023-12-14 19:18:10 +0100

    add second example

--- transformacje/trans.py
+++ transformacje/trans.py
@@ -1,6 +1,5 @@
 from pyproj import Transformer
 
-
 def dms(x):
     degrees = x // 1
     temp = (x - degrees) * 60
@@ -14,10 +13,27 @@ def decimal_degrees(degrees, minutes, sec):
 
 
 if __name__ == '__main__':
-    lat = decimal_degrees(50, 3, 56.71715)
-    long = decimal_degrees(19, 55, 12.92679)
-    transformer = Transformer.from_crs('epsg:4326', 'epsg:2178')
-    x, y = transformer.transform(lat, long)
 
-    print(f'Współrzędna X(N): {x:.3f}')
-    print(f'Współrzędna Y(E): {y:.3f}')
+    #Ex1: WGS 84 [EPSG4326] to 2000 [EPSG2178]
+    # lat = decimal_degrees(50, 3, 56.71715)
+    # long = decimal_degrees(19, 55, 12.92679)
+    # transformer1 = Transformer.from_crs('epsg:4326', 'epsg:2178')
+    # x0, y0 = transformer1.transform(lat, long)
+    #
+    # print(f'Współrzędna X(N): {x0:.3f}')
+    # print(f'Współrzędna Y(E): {y0:.3f}')
+
+
+    #Ex2: ECEF WGS84 [EPSG4978] to WGS84 [EPSG4326]
+    transformer2 = Transformer.from_crs("EPSG:4978", "EPSG:4326", always_xy=True)
+
+    # Example coordinates in EPSG:4978
+    x, y, z = 3912239.4942, 1357006.7149, 4835451.9071  # Replace these with your coordinates
+
+    # Transform the coordinates
+    lon, lat, alt = transformer2.transform(x, y, z)
+
+    # Print the transformed coordinates
+    print(f'X: {x}, Y: {y}, Z: {z}')
+    print(f"Latitude: {lat:.10f}\u00B0, Longitude: {lon:.10f}\u00B0, Altitude: {alt:.4f}m")
+    print(f'Latitude {dms(lat)[0]:.0f}\u00B0{dms(lat)[1]:.0f}\'{dms(lat)[2]:.5f}\", Longitude: {dms(lon)[0]:.0f}\u00B0{dms(lon)[1]:.0f}\'{dms(lon)[2]:.5f}\", Altitude: {alt:.4f}m ')
